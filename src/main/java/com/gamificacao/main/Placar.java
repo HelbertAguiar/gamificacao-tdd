dab5395a648ad05c225058e0754d879da33987c3
Helbert <helbert.aguiar@hotmail.com>
2026-06-22 22:34:13 -0300

    add method to retrieve points for a user in Placar class

--- src/main/java/com/gamificacao/main/Placar.java
+++ src/main/java/com/gamificacao/main/Placar.java
@@ -2,6 +2,8 @@ package com.gamificacao.main;
 
 import com.gamificacao.main.RepositorioPontos;
 
+import java.util.Map;
+
 public class Placar {
 
     private final RepositorioPontos repositorio;
@@ -13,4 +15,19 @@ public class Placar {
     public void registrarPontos(String usuario, String tipo, int quantidade) {
         repositorio.registrarPontos(usuario, tipo, quantidade);
     }
+
+    public Map<String, Integer> recuperarPontosDoUsuario(String usuario) {
+        Map<String, Integer> pontos = new java.util.HashMap<>();
+
+        for (String tipo : repositorio.recuperarTiposDePonto()) {
+            int quantidade = repositorio.recuperarPontos(usuario, tipo);
+
+            if (quantidade > 0) {
+                pontos.put(tipo, quantidade);
+            }
+        }
+
+        return pontos;
+    }
+
 }
