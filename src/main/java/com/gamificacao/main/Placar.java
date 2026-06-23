3c26b138836bb96133c5a442acb6e5a70a3f6472
Helbert <helbert.aguiar@hotmail.com>
2026-06-22 22:30:40 -0300

    add Placar class constructor and method to register points

--- src/main/java/com/gamificacao/main/Placar.java
+++ src/main/java/com/gamificacao/main/Placar.java
@@ -1,4 +1,16 @@
 package com.gamificacao.main;
 
+import com.gamificacao.main.RepositorioPontos;
+
 public class Placar {
+
+    private final RepositorioPontos repositorio;
+
+    public Placar(RepositorioPontos repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void registrarPontos(String usuario, String tipo, int quantidade) {
+        repositorio.registrarPontos(usuario, tipo, quantidade);
+    }
 }
