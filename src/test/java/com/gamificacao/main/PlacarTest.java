8dbc447359872e92ce45b74d963cc424a5cc4a39
Helbert <helbert.aguiar@hotmail.com>
2026-06-22 22:32:58 -0300

    add test to retrieve all points for a user in Placar class

--- src/test/java/com/gamificacao/main/PlacarTest.java
+++ src/test/java/com/gamificacao/main/PlacarTest.java
@@ -1,6 +1,9 @@
 package com.gamificacao.main;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlacarTest {
@@ -14,4 +17,22 @@ class PlacarTest {
         assertEquals("estrela", repositorio.ultimoTipoRegistrado);
         assertEquals(10, repositorio.ultimaQuantidadeRegistrada);
     }
+
+    @Test
+    void deveRetornarTodosOsPontosDeUmUsuario() {
+        RepositorioDePontosMock repositorio = new RepositorioDePontosMock();
+        Placar placar = new Placar(repositorio);
+        repositorio.registrarPontos("helbert", "moeda", 20);
+        repositorio.registrarPontos("helbert", "estrela", 25);
+        repositorio.registrarPontos("ana", "moeda", 100);
+        Map<String, Integer> pontos = placar.recuperarPontosDoUsuario("helbert");
+        assertEquals(
+            Map.of(
+                    "moeda", 20,
+                    "estrela", 25
+            ),
+            pontos
+        );
+    }
+
 }
